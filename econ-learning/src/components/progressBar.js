c3e6b9476edd7e63324770c373aeee8a0a971344
Araf Rahman <arahman.usa500@gmail.com>
2021-03-29 13:14:11 -0400

    Added Progress bar

---
+++ econ-learning/src/components/progressBar.js
@@ -0,0 +1,53 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Styled from "styled-components";
+
+
+const Container = Styled.div`
+  progress {
+    margin-right: 8px;
+  }
+
+  progress[value] {
+    width: ${props => props.width};
+
+    -webkit-appearance: none;
+    appearance: none;
+  }
+
+  progress[value]::-webkit-progress-bar {
+    height: 10px;
+    border-radius: 20px;
+    background-color: #eee;
+  }  
+
+  progress[value]::-webkit-progress-value {
+    height: 10px;
+    border-radius: 20px;
+    background-color: ${props => props.color};
+  }
+`;
+
+const ProgressBar = ({ value, max, color, width }) => {
+  return (
+    <Container color={color} width={width}>
+      <progress value={value} max={max} align="left"/>
+      <span>{(value / max) * 100}%</span>
+    </Container>
+  );
+};
+
+ProgressBar.propTypes = {
+  value: PropTypes.number.isRequired,
+  max: PropTypes.number,
+  color: PropTypes.string,
+  width: PropTypes.string
+};
+
+ProgressBar.defaultProps = {
+  max: 100,
+  color: "lightBlue",
+  width: "250px"
+};
+
+export default ProgressBar;
--- econ-learning/src/pages/Welcome.js
+++ econ-learning/src/pages/Welcome.js
@@ -2,6 +2,8 @@ import React, { useState, useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 import './Welcome.css';
 import { CredentialsContext } from "../App";
+import DragChart from '../components/TestingDragChart'
+import ModuleProgress from '../components/ModuleProgress'
 
 export const handleErrors = async (response) => {
   if (!response.ok) {
@@ -18,6 +20,7 @@ export default function Welcome() {
   };
 
   return (
+    <>
     <div >
       <h1>Welcome {credentials && credentials.username}</h1>
       {!credentials && <Link class="registerBt" to="/register">Register</Link>}
@@ -25,7 +28,13 @@ export default function Welcome() {
       {!credentials && <Link class="loginBt" to="/login">Login</Link>}
       <div class ="wrapper">
       {credentials && <button class="logoutBtn" onClick={logout}>Logout</button>}
+      {credentials && `Tokens:`}
       </div>
     </div>
+    <br/><br/><br/>
+    {credentials && <ModuleProgress m1={50} m2={40} m3={20} m4={80} m5={100} m6={90} m7={10} m8={50}/> }
+    <br/><br/><br/>
+    <DragChart/> 
+    </>
   );
 }
